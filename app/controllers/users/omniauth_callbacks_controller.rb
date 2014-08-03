e32df1cbe1c18949c0ba33aba114b135bb5c9b3d
Spencer Brown <me@spencer.sx>
2014-08-03 10:04:11 -0700

    log to server if user does not persist

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -6,6 +6,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @user
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
+      logger.debug('User not persisted!')
       session["devise.facebook_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
