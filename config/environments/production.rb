4b56b073bea030190a9a7da41efc1792cdba91d4
Spencer Brown <me@spencer.sx>
2014-08-03 00:33:09 -0700

    fix prod environment config typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,5 +81,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  onfig.action_mailer.default_url_options = { host: 'smev.heroku.com' }
+  config.action_mailer.default_url_options = { host: 'lodgeapp.heroku.com' }
 end
